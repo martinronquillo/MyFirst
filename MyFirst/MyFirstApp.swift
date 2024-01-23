d59dd0a4ed24f34fbcf452e1c5e9249ae6b7032e
Martin Ronquillo <martinronquillo1998@gmail.com>
2024-01-23 14:14:50 -0800

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Marty on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
